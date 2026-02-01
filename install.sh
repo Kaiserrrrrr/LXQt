f3cca44050bed1d4f621fbe64f259983dafab2c8
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-02-01 16:27:34 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo pacman -Syu && \
+sudo pacman -S --noconfirm xorg-server lightdm lightdm-gtk-greeter \
+openbox lxqt-session lxqt-panel lxqt-config lxqt-qtplugin \
+pcmanfm-qt qterminal breeze-icons menu-cache && \
+sudo pacman -Scc --noconfirm && \
+sudo systemctl enable lightdm && \
+reboot
