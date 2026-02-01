dcb402ebe51da42fbe4008a1385548e1abdde5d1
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-02-01 17:00:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 sudo pacman -Syu && \
-sudo pacman -S --noconfirm xorg-server lightdm lightdm-gtk-greeter \
-openbox lxqt-session lxqt-panel lxqt-config lxqt-qtplugin \
-pcmanfm-qt qterminal breeze-icons menu-cache && \
+sudo pacman -S --noconfirm xorg-server lightdm lightdm-gtk-greeter openbox lxqt-session lxqt-panel lxqt-config lxqt-qtplugin pcmanfm-qt qterminal breeze-icons menu-cache && \
 sudo pacman -Scc --noconfirm && \
 sudo systemctl enable lightdm && \
 reboot
