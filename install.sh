8017f8f530b418560751c5b0ce6dfd7dd1d7b471
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-02-13 23:57:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo pacman -Syu && \
-sudo pacman -S --noconfirm xorg-server lightdm lightdm-gtk-greeter openbox lxqt-session lxqt-panel lxqt-config lxqt-qtplugin pcmanfm-qt qterminal breeze-icons menu-cache fastfetch && \
+sudo pacman -S --noconfirm xorg-server lightdm lightdm-gtk-greeter openbox lxqt-session lxqt-panel lxqt-config lxqt-qtplugin pcmanfm-qt qterminal breeze-icons menu-cache falkon fastfetch && \
 sudo pacman -Scc --noconfirm && \
 sudo systemctl enable lightdm && \
+mkdir -p ~/.config/pcmanfm-qt/lxqt/; echo -e "[General]\ntrust_all_desktop_files=true" >> ~/.config/pcmanfm-qt/lxqt/settings.conf && \
 reboot
