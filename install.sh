cf6d7b5046f20beffd9b5822dd55fbb4d5728ed0
NotKaiser <97164185+Kaiserrrrrr@users.noreply.github.com>
2026-02-15 17:10:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo pacman -Syu --noconfirm && sudo pacman -S --noconfirm xorg-server lightdm lightdm-gtk-greeter lxqt-session lxqt-panel lxqt-config lxqt-qtplugin lxqt-powermanagement lxqt-notificationd lxqt-policykit pcmanfm-qt qterminal breeze-icons falkon fastfetch htop openbox && sudo systemctl enable lightdm && sudo pacman -Scc --noconfirm && reboot
+sudo pacman -Syu --noconfirm && sudo pacman -S --noconfirm xorg-server lightdm lightdm-gtk-greeter lxqt-session lxqt-panel lxqt-config lxqt-qtplugin lxqt-powermanagement lxqt-notificationd lxqt-policykit pcmanfm-qt qterminal breeze-icons falkon neovim fastfetch htop openbox && sudo systemctl enable lightdm && sudo pacman -Scc --noconfirm && reboot
